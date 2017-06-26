fab26ad5d7952265c1a9f222b99448d86a015e41
Jake T <jaketepper@gmail.com>
2017-06-26 17:37:27 -0400

    adding files

--- src/main/java/com/theironyard/charlotte/IntroWebsite/IntroWebsiteApplication.java
+++ src/main/java/com/theironyard/charlotte/IntroWebsite/IntroWebsiteApplication.java
@@ -2,11 +2,52 @@ package com.theironyard.charlotte.IntroWebsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class IntroWebsiteApplication {
+//	@Autowired
+//	PasswordEncoder encoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntroWebsiteApplication.class, args);
 	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+//
+//	@Bean
+//	public PasswordEncoder encoder() {
+//		return new BCryptPasswordEncoder();
+//	}
+//
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http
+//				.authorizeRequests()
+//				.antMatchers("/chatroom", "/adminchat").authenticated()
+//				.anyRequest().permitAll()
+//				.and()
+//				.formLogin()
+//				.loginPage("/login")
+//				.permitAll()
+//				.and()
+//				.logout()
+//				.logoutUrl("/logout")
+//				.permitAll()
+//				.and()
+//				.csrf().disable();
+//	}
+//	@Autowired
+//	public void configureAuth(AuthenticationManagerBuilder auth, DataSource dataSource) throws Exception {
+//		auth.jdbcAuthentication()
+//				.passwordEncoder(encoder)
+//				.dataSource(dataSource)
+//				.usersByUsernameQuery("select username, password, enabled from users where username = ?")
+//				.authoritiesByUsernameQuery("select users.username, authorities.role_name from users inner join authorities on users.id = authorities.user_id where users.username = ?");
+//	}
 }
--- src/main/java/com/theironyard/charlotte/IntroWebsite/controllers/IntroWebsiteController.java
+++ src/main/java/com/theironyard/charlotte/IntroWebsite/controllers/IntroWebsiteController.java
@@ -1,29 +1,23 @@
 package com.theironyard.charlotte.IntroWebsite.controllers;
 
-import com.theironyard.charlotte.IntroWebsite.entities.Message;
 import com.theironyard.charlotte.IntroWebsite.models.ImgurData;
 import com.theironyard.charlotte.IntroWebsite.models.WeatherData;
-import com.theironyard.charlotte.IntroWebsite.repositories.MessageRepo;
 import com.theironyard.charlotte.IntroWebsite.services.ImageService;
-import com.theironyard.charlotte.IntroWebsite.services.MessageService;
 import com.theironyard.charlotte.IntroWebsite.services.WeatherService;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.Collections;
-import java.util.List;
-
 @Controller
 public class IntroWebsiteController {
     ImageService imageService;
     WeatherService weatherService;
-    MessageService msg;
-    MessageRepo messageRepo;
 
+    public IntroWebsiteController(ImageService imageService, WeatherService weatherService) {
+        this.imageService = imageService;
+        this.weatherService = weatherService;
+    }
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String homePage(Model model) {
@@ -61,24 +55,24 @@ public class IntroWebsiteController {
         model.addAttribute("allAlbums", true);
         return "albums";
     }
-
-    @RequestMapping(path = "/chatroom", method = RequestMethod.GET)
-    public String chatroom(Model model) {
-        List<Message> allMessages= messageRepo.findFirst5ByOrderByIdDesc();
-        Collections.reverse(allMessages);
-        model.addAttribute("messages", allMessages);
-        return "chatroom";
-    }
-    @RequestMapping(path = "/addmessage", method = RequestMethod.POST)
-    public String messagePost(Model model, String message) {
-        Authentication currentAuth = SecurityContextHolder.getContext().getAuthentication();
-        String currentUser = currentAuth.getName();
-        msg.createMessage(currentUser, message);
-        return "redirect:/chatroom";
-    }
-
-    @RequestMapping(path = "/adminchat", method = RequestMethod.GET)
-    public String adminPage(Model model) {
-        return "adminchat";
-    }
+//
+//    @RequestMapping(path = "/chatroom", method = RequestMethod.GET)
+//    public String chatroom(Model model) {
+//        List<Message> allMessages= messageRepo.findFirst5ByOrderByIdDesc();
+//        Collections.reverse(allMessages);
+//        model.addAttribute("messages", allMessages);
+//        return "chatroom";
+//    }
+//    @RequestMapping(path = "/addmessage", method = RequestMethod.POST)
+//    public String messagePost(Model model, String message) {
+//        Authentication currentAuth = SecurityContextHolder.getContext().getAuthentication();
+//        String currentUser = currentAuth.getName();
+//        msg.createMessage(currentUser, message);
+//        return "redirect:/chatroom";
+//    }
+//
+//    @RequestMapping(path = "/adminchat", method = RequestMethod.GET)
+//    public String adminPage(Model model) {
+//        return "adminchat";
+//    }
 }
